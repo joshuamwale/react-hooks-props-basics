19309bd2dc37bac48329ba0c1c4cab01efc4bd30
Joshua <mwalejoshua89@gmail.com>
2022-07-16 17:28:01 +0300

    Done with the code along

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,14 +1,19 @@
 import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
+import Score from "./Score";
 
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration. He is the best among many."
+       isPublished={true}
+       minutesToRead={1} />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="He is just so excellent in his delivery. - Joshua Mwale"/>
+      <Score scoreText="I give the oration a score of 9. - JM"/>
     </div>
   );
 }
---
+++ src/components/Score.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Score(props) {
+  return <div className="score">{props.scoreText}</div>;
+}
+
+export default Score;
